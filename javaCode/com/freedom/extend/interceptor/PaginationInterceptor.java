0317118404833704f5acad2868f44cf76134dce6
maomao <maomao@202.99.254.232>
2013-01-08 17:29:22 +0800

    完善分页拦截方法

--- javaCode/com/freedom/extend/interceptor/PaginationInterceptor.java
+++ javaCode/com/freedom/extend/interceptor/PaginationInterceptor.java
@@ -16,6 +16,7 @@ import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.RowBounds;
 
 import com.freedom.extend.data.DataDialect;
+import com.freedom.extend.data.MysqlDialect;
 import com.freedom.extend.data.OracleDialect;
 
 @Intercepts({@Signature(type=StatementHandler.class,method="prepare",args={Connection.class})})
@@ -24,40 +25,43 @@ public class PaginationInterceptor implements Interceptor
     private static Log log = LogFactory.getLog(PaginationInterceptor.class);
 
     public Object intercept(Invocation invocation) throws Throwable 
-    {
-
+     {
         StatementHandler statementHandler = (StatementHandler)invocation.getTarget();  
         MetaObject metaStatementHandler = MetaObject.forObject(statementHandler);  
-          
+        
         RowBounds rowBounds = (RowBounds)metaStatementHandler.getValue("delegate.rowBounds");  
-        if(rowBounds == null || rowBounds == RowBounds.DEFAULT){  
+        if(rowBounds == null || rowBounds == RowBounds.DEFAULT)
+           {  
             return invocation.proceed();  
-        }  
-
+           }  
         String originalSql = (String)metaStatementHandler.getValue("delegate.boundSql.sql");  
-          
-
+        
         Configuration configuration = (Configuration)metaStatementHandler.getValue("delegate.configuration");  
-          
+
         DataDialect.Type databaseType  = null;  
-        try{  
+        try
+           {  
             databaseType = DataDialect.Type.valueOf(configuration.getVariables().getProperty("dialect").toUpperCase());  
-        } catch(Exception e){  
-            //ignore  
-        }  
-        if(databaseType == null){  
+           } 
+        catch(Exception e)
+           {  
+        	log.error(e);
+           }  
+        if(databaseType == null)
+           {  
             throw new RuntimeException("the value of the dialect property in configuration.xml is not defined : " + configuration.getVariables().getProperty("dialect"));  
-        }  
+           }  
         DataDialect dialect = null;  
-        switch(databaseType){  
+        switch(databaseType)
+           {  
             case ORACLE:  
                 dialect = new OracleDialect();
                 break;  
-            case MYSQL://需要实现MySQL的分页逻辑  
-                break;  
-                  
-        }  
-
+            case MYSQL:
+            	 dialect = new MysqlDialect();
+                break;
+           }  
+        System.out.println("SQL:" + dialect.getLimitString(originalSql, rowBounds.getOffset(), rowBounds.getLimit()));
         metaStatementHandler.setValue("delegate.boundSql.sql", dialect.getLimitString(originalSql, rowBounds.getOffset(), rowBounds.getLimit()) );  
         metaStatementHandler.setValue("delegate.rowBounds.offset", RowBounds.NO_ROW_OFFSET );  
         metaStatementHandler.setValue("delegate.rowBounds.limit", RowBounds.NO_ROW_LIMIT );  
@@ -65,14 +69,15 @@ public class PaginationInterceptor implements Interceptor
         return invocation.proceed();  
     }
 
-    @Override
-    public Object plugin(Object target) {
+ 
+   @Override
+   public Object plugin(Object target) {
         // TODO Auto-generated method stub
         return Plugin.wrap(target, this);
     }
 
-    @Override
-    public void setProperties(Properties arg0) {
+   @Override
+   public void setProperties(Properties arg0) {
         // TODO Auto-generated method stub
         
     }
